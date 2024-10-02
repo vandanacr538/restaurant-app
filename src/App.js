137708f0db3e0ecaaf183845c1ef6e95cbbd5114
vandanacr538 <vandanacr538@gmail.com>
2024-10-02 16:57:07 +0530

    Added Meals Summary

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Layout/Header";
+import MealsSummary from "./components/Meals/MealsSummary";
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <MealsSummary />
     </React.Fragment>
   );
 }
