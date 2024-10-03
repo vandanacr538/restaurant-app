c3c40769538113a1efba7a70256c76bff6e3489a
vandanacr538 <vandanacr538@gmail.com>
2024-10-03 20:25:52 +0530

    Added CartContext & shown cartItems Count

---
+++ src/store/CartProvider.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import CartContext from "./cart-context";
+
+const CartProvider=(props)=>{
+    const [items, setItems] = useState([]);
+    const [totalAmount, setTotalAmount] = useState(0);
+
+    const addItemToCartHandler=(item)=>{
+        setItems((prevItems)=>{
+            return [...prevItems, item];
+        });
+        setTotalAmount((prevAmount)=>{
+            return prevAmount+item.price*item.amount;
+        });
+    }
+
+    const removeItemFromCartHandler=(id)=>{
+        setItems((prevItems)=>{
+            return [prevItems.filter((item)=>item.id!==id)];
+        });
+        const itemToRemove=items.find(item=>item.id===id);
+        setTotalAmount((prevAmount)=>{
+            return prevAmount-itemToRemove.price*itemToRemove.amount;
+        });
+    }
+
+    const cartContext={
+        items: items,
+        totalAmount: totalAmount,
+        addItem: addItemToCartHandler,
+        removeItem: removeItemFromCartHandler,
+    }
+
+    return (
+        <CartContext.Provider value={cartContext}>
+            {props.children}
+        </CartContext.Provider>
+    )
+}
+
+export default CartProvider;
---
+++ src/store/cart-context.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const CartContext=React.createContext({
+    items: [{
+        id: "1",
+        name: "Sushi",
+        description: "Finsest fish and veggies",
+        price: 22.99,
+        amount: 1
+    }],
+    totalAmount: 0,
+    addItem: (item)=>{ },
+    removeItem: (id)=>{ }
+});
+
+export default CartContext;
